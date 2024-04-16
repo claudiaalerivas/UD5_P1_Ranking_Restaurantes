31c8dff056df4d3dfc826d2231e88e1366746a69
claudiaalerivas <usr3860@salesianos-lacuesta.net>
2024-04-16 11:57:41 +0100

    feat(Restaurante): añadidos atributos de la clase

---
+++ src/org/ripadbaisor/elementos/Restaurante.java
@@ -0,0 +1,11 @@
+package org.ripadbaisor.elementos;
+
+public class Restaurante {
+  private String nombre = "";
+  private String localizacion = "";
+  private String horario = "";
+  private float puntuacion;
+
+  
+
+}
