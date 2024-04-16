29c706f37bb5ff73fcd45087ade01b823680dc38
claudiaalerivas <usr3860@salesianos-lacuesta.net>
2024-04-16 12:00:51 +0100

    feat(Restaurante): constructor Restaurante

--- src/org/ripadbaisor/elementos/Restaurante.java
+++ src/org/ripadbaisor/elementos/Restaurante.java
@@ -5,6 +5,13 @@ public class Restaurante {
   private String localizacion = "";
   private String horario = "";
   private float puntuacion;
+  
+  public Restaurante(String nombre, String localizacion, String horario, float puntuacion) {
+    this.nombre = nombre;
+    this.localizacion = localizacion;
+    this.horario = horario;
+    this.puntuacion = puntuacion;
+  }
 
   
 
