6da95dc30cc6f37d14e81ae9a40242ab558c30c5
claudiaalerivas <usr3860@salesianos-lacuesta.net>
2024-04-16 13:59:05 +0100

    feat(Main): Añadido menu en clase Main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
-import java.util.ArrayList;
 
 import org.ripadbaisor.elementos.Restaurante;
-import org.ripadbaisor.gestor.Gestor;
 import org.ripadbaisor.menu.Menu;
 
 
@@ -9,7 +7,6 @@ public class Main {
     public static void main(String[] args) throws Exception {
       Menu menu = new Menu();
       menu.mostrarMenu();
-      Gestor gestor = new Gestor();
       
     }
 }
