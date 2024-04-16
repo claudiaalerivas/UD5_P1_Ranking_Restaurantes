e14ee8f6b1db30d7814c14de09672c657a07b5a8
claudiaalerivas <usr3860@salesianos-lacuesta.net>
2024-04-16 12:02:05 +0100

    feat(Restaurante): metodos de acceso creados

--- src/org/ripadbaisor/elementos/Restaurante.java
+++ src/org/ripadbaisor/elementos/Restaurante.java
@@ -13,6 +13,38 @@ public class Restaurante {
     this.puntuacion = puntuacion;
   }
 
+  public String getNombre() {
+    return nombre;
+  }
+
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
+
+  public String getLocalizacion() {
+    return localizacion;
+  }
+
+  public void setLocalizacion(String localizacion) {
+    this.localizacion = localizacion;
+  }
+
+  public String getHorario() {
+    return horario;
+  }
+
+  public void setHorario(String horario) {
+    this.horario = horario;
+  }
+
+  public float getPuntuacion() {
+    return puntuacion;
+  }
+
+  public void setPuntuacion(float puntuacion) {
+    this.puntuacion = puntuacion;
+  }
+
   
 
 }
